068c55ba4781a7cd7290baa0f65898517156ecda
Janaina da Silva Rodrigues <145395931+dasilvajanaine@users.noreply.github.com>
2025-04-15 14:45:17 -0300

    1bv bv5

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
     const alternaContraste = document.getElementById('alterna-contraste');
-
     
+
     let tamanhoAtualFonte = 1;
     
     aumentaFonteBotao.addEventListener('click', function(){
@@ -26,10 +26,10 @@ document.addEventListener('DOMContentLoaded', function(){
     document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     });
 
-    alternaContraste.addEventListener('click' function(){
-        Document.body.classList.toggle('alto-contraste')
-
-    })
+alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste');
 
 
-    });
+});
+
+});
